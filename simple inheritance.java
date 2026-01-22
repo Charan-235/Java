f02394902f5080ff1a7981c29a606ea3db9d601e
KrishnaCharan <krishnacharan689@gmail.com>
2026-01-22 09:50:24 +0530

    Implement simple inheritance with vehicle class

---
+++ simple inheritance.java
@@ -0,0 +1,20 @@
+class vehicle
+{
+  void vdisplay()
+  {
+    System.out.println("vehicle");
+  }
+}
+class R192311316 extends vehicle
+{
+  void rdisplay()
+  {
+    System.out.println("2 wheeler");
+  }
+  public static void main(String[] args)
+  {
+  R192311316 R = new R192311316();
+  R.vdisplay();
+  R.rdisplay();
+  }
+}
